63da444b52968f43b09aa1a2e1a3f27480ffa760
Austin <austinccy81@gmail.com>
2022-07-12 21:29:00 +0100

    update page title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const App = () => {
   const getRecipes = async () =>{
     const response = await fetch (`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json();
-    console.log(data)
     setRecipes (data.hits);
   }
 
